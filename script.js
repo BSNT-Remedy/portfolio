c18f15ff0d95c46014642d439d5775753da8b06b
Allan <allanjaybusante3@gmail.com>
2025-09-23 11:06:15 +0800

    Style Education and Skill section

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // alert("Welcome to my portfolio!");
+
+document.querySelectorAll('.skill').forEach(img => {
+    const delay = (Math.random() * 2).toFixed(2); 
+    img.style.animationDelay = `${delay}s`;
+});
